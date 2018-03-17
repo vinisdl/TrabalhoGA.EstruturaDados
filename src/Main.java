5a3f56c0999751d370cc9405b141b7b73307a658
vinisdl <vinisdl@hotmail.com>
2018-03-16 21:17:24 -0300

    adicionando metodo de inserção avl

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
 		BinarySearchTreeAVL<Integer, Integer> avl = new BinarySearchTreeAVL<Integer, Integer>();
 		BinarySearchTree<Integer, Integer> bst = new BinarySearchTree<Integer, Integer>();
-
-		for (int i = 0; i < 6; i++) {
-			avl.insert(i, i);
-			bst.insert(i, i);
-		}
-		
+			
 		System.out.println(bst.toString());
 		
 		System.out.println(avl.toString());
-
+		
+		int[] values = new int[5000];
+		Random rand = new Random();
+		for (int i = 0; i < 5000; i++) {
+			values[i] =  rand.nextInt((0 - 5000) + 1) + 1;
+		}
+		
+		long insertAvl = System.currentTimeMillis();		
+		inserItems(avl,values);		
+		insertAvl = System.currentTimeMillis() - insertAvl;
+		
 	}
+	
+	
+	private static void inserItems(IBinarySearchTreeADT<Integer, Integer>  tree, int[] values) {
+		for (int i = 0; i < values.length; i++) {
+			tree.insert(values[i], values[i]);
+		}
+	}
+	
 }	
